7068f92b4d3423faffb3ab82f0f5478e08f5da64
takotakot <takotakot@users.noreply.github.com>
2025-05-06 17:02:58 +0900

    feat: Add Cloud Run service for structured logging

--- structured-logging/terraform/environments/stg/main.tf
+++ structured-logging/terraform/environments/stg/main.tf
@@ -50,3 +50,98 @@ resource "google_secret_manager_secret" "rails_master_key" {
 
   depends_on = [google_project_service.services["secretmanager.googleapis.com"]]
 }
+
+# Cloud Run Service
+resource "google_service_account" "structured-logging" {
+  account_id   = "structured-logging"
+  display_name = "Structured Logging Service Account"
+  project      = var.project_id
+
+  depends_on = [google_project_service.services["iam.googleapis.com"]]
+}
+
+resource "google_project_iam_member" "secret_accessor" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.structured-logging.email}"
+
+  depends_on = [google_project_service.services["run.googleapis.com"]]
+}
+
+resource "google_cloud_run_v2_service" "structured-logging" {
+  name     = "structured-logging"
+  location = var.region
+
+  template {
+    containers {
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.structured-logging.repository_id}/structured-logging:latest"
+
+      ports {
+        container_port = 80
+      }
+
+      env {
+        name = "RAILS_MASTER_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.rails_master_key.secret_id
+            version = "latest"
+          }
+        }
+      }
+
+      startup_probe {
+        failure_threshold = 3
+        period_seconds    = 5
+        timeout_seconds   = 5
+        http_get {
+          path = "/up"
+        }
+      }
+
+      liveness_probe {
+        failure_threshold = 3
+        period_seconds    = 5
+        timeout_seconds   = 5
+        http_get {
+          path = "/up"
+        }
+      }
+    }
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 2
+    }
+
+    # invoker_iam_disabled = true
+
+    service_account = google_service_account.structured-logging.email
+  }
+
+  invoker_iam_disabled = true
+
+  lifecycle {
+    ignore_changes = [
+      #   template[0].containers[0].env[0].value_source[0].secret_key_ref[0].version,
+      template[0].containers[0].env,
+      template[0].containers[0].image,
+    ]
+  }
+
+  #   traffic {
+  #     latest_revision = true
+  #     percent         = 100
+  #   }
+
+  depends_on = [
+    google_project_service.services["run.googleapis.com"],
+    google_project_service.services["servicenetworking.googleapis.com"],
+    google_project_service.services["cloudbuild.googleapis.com"],
+    google_project_iam_member.secret_accessor,
+  ]
+}
+
+output "cloud_run_service_url" {
+  value = google_cloud_run_v2_service.structured-logging.uri
+}
