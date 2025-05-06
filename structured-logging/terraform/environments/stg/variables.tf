c10603da7a1eff2b6df4409ac0e4afb7a2a352ae
takotakot <takotakot@users.noreply.github.com>
2025-05-06 15:49:29 +0900

    chore: Init Terraform

---
+++ structured-logging/terraform/environments/stg/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type    = string
+  default = ""
+}
+
+variable "region" {
+  type    = string
+  default = "asia-northeast1"
+}
+
+variable "backend_bucket_name" {
+  type    = string
+  default = ""
+}
