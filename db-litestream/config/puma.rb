b912f272b7d9d1fad6367d370d9b6b28114bad98
takotakot <takotakot@users.noreply.github.com>
2025-05-18 00:33:32 +0900

    feat: Add plugin litestream to `puma.rb` to stream database changes

--- db-litestream/config/puma.rb
+++ db-litestream/config/puma.rb
@@ -39,3 +39,6 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Run litestream only in production.
+plugin :litestream if ENV.fetch("RAILS_ENV", "production") == "production"
