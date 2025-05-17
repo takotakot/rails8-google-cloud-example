8ee3afad8916c002bf715aabebc5af56d0d999b2
takotakot <takotakot@users.noreply.github.com>
2025-05-17 17:11:01 +0900

    feat: Add k6 script and results

---
+++ db-cloud-storage/k6/script.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 150,
+  duration: '3s',
+};
+
+export default function() {
+  const res = http.post('https://db-cloud-storage-886786442757.asia-northeast1.run.app/count_up');
+  check(res, { "status is 200": (res) => res.status === 200 });
+  // sleep(1);
+}
